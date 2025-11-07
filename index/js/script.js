a0213bcf8cd415ac48f12fdbeb2a877649f81185
Nathan <nathan162318@gmail.com>
2025-11-07 17:44:46 +0100

    MAJ

---
+++ index/js/script.js
@@ -0,0 +1,5 @@
+console.log("%c© 2025 - Nathan The Coder", "background: #282c34; color: #98c379; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%cSites du Nathan", "background: #282c34; color: #61afef; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%cCe site rassemble tous les sites que Nathan utilise.","background: #282c34; color: #61dafb; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%chttps://sites-nathan.netlify.app", "background: #282c34; color: #e06c75; padding: .5em 1em; border-radius: 5px; font-weight: bold;");
+console.log("%cMade with 🕑 and 💖 by Nathan J. – Last update : 07/11/2025","background: #282c34; color: #c678dd; padding: .5em 1em; border-radius: 5px; font-weight: bold;")
